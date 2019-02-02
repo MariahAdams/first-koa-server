6d5e3ae1b822f62c0fd8149a88def7a52c1ae2bb
MariahAdams <mariah.adams23@yahoo.com>
2019-02-02 15:30:31 -0800

    hello world

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 require('dotenv').config();
-const { createServer } = require('http');
 const app = require('./lib/app');
-const connect = require('./lib/utils/connect');
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
 
-connect(MONGODB_URI);
-
-const server = createServer(app);
-
-server.listen(PORT, () => {
+if(!module.parent) app.listen(PORT, () => {
     console.log(`Server jammin on ${PORT}`);
 });
